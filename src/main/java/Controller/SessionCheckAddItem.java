1160ae2f1e8db2c9b86c43f9aecfb245ca54340b
Asmita Tandel <asmitatandel286@googlemail.com>
2023-09-04 17:54:38 +0530

    projects

--- src/main/java/Controller/SessionCheckAddItem.java
+++ src/main/java/Controller/SessionCheckAddItem.java
@@ -1,8 +1,24 @@
 package Controller;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/additemsession")
 public class SessionCheckAddItem  extends HttpServlet{
-	doF
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// session verification
+				if (req.getSession().getAttribute("admin") == null) {
+					resp.getWriter().print("<h1 style='color:red'>Invalid session</h1>");
+					req.getRequestDispatcher("LoginPage.html").include(req, resp);
 
+				} else {
+					req.getRequestDispatcher("AddItem.html").include(req, resp);
+	}
+	}
 }
